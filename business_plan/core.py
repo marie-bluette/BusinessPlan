7efeb219528c2f44c4eea6351b8601854af84a17
dumouchel <dumouchel@dessia.tech>
2020-10-04 18:27:52 +0200

    Avancement bp

--- business_plan/core.py
+++ business_plan/core.py
@@ -13,28 +13,77 @@ class Employee(DessiaObject):
     def __init__(self, profit_center: bool=False,
                  salary: float=None,
                  general_expenses: float=0,
-
+                 hiring_year: float=None,
+                 exit_year: float=None,
                  name:str=''):
 
         DessiaObject.__init__(self, name=name)
+        self.exit_year = exit_year
+        self.hiring_year = hiring_year
         self.general_expenses = general_expenses
         self.salary = salary
         self.profit_center = profit_center
 
-class Revenue(DessiaObject):
+    def cost(self, year):
+        if year < self.hiring_year or year > self.exit_year:
+            return 0
+        else:
+            return self.salary + self.general_expenses
+
+class EmployeeSale(Employee):
+    def __init__(self, salary: float,
+                 general_expenses: float, hiring_year: float=None, name:str=''):
+        Employee.__init__(self, salary=salary, general_expenses=general_expenses,
+                          profit_center=True, hiring_year=hiring_year, name=name)
+
+class EmployeeSupport(Employee):
+    def __init__(self, salary: float,
+                 general_expenses: float, hiring_year: float=None, name:str=''):
+        Employee.__init__(self, salary=salary, general_expenses=general_expenses,
+                          profit_center=False, hiring_year=hiring_year, name=name)
+
+class EmployeeTalker(Employee):
+    def __init__(self, salary: float,
+                 general_expenses: float, hiring_year: float=None, name:str=''):
+        Employee.__init__(self, salary=salary, general_expenses=general_expenses,
+                          profit_center=False, hiring_year=hiring_year, name=name)
+
+class EmployeeMaker(Employee):
+    def __init__(self, salary: float,
+                 general_expenses: float, hiring_year: float=None, name:str=''):
+        Employee.__init__(self, salary=salary, general_expenses=general_expenses,
+                          profit_center=False, hiring_year=hiring_year, name=name)
+
+class Evolution(DessiaObject):
     _standalone_in_db = True
     _generic_eq = True
     _non_serializable_attributes = []
     _non_eq_attributes = ['name']
     _non_hash_attributes = ['name']
 
-    def __init__(self, revenues: Dict[int, float],
+    def __init__(self, evolutions: Dict[int, float]=None,
                  name: str = ''):
         DessiaObject.__init__(self, name=name)
-        self.revenues = revenues
+        if evolutions is None:
+            self.evolutions = {}
+        else:
+            self.evolutions = evolutions
 
-    def __add__(self, other_revenue):
-        year1
+    def __add__(self, other_evolution):
+        year1 = list(self.evolutions.keys())
+        year2 = list(other_evolution.evolutions.keys())
+        all_year = year1
+        for y in year2:
+            if y not in all_year:
+                all_year.append(y)
+        evolutions = {}
+        for y in all_year:
+            evolutions[y] = 0
+            if y in self.evolutions:
+                evolutions[y] += self.evolutions[y]
+            if y in other_evolution.evolutions:
+                evolutions[y] += other_evolution.evolutions[y]
+        return evolutions
 
 class MainDivision(DessiaObject):
     _standalone_in_db = True
@@ -61,14 +110,27 @@ class GeographicArea(DessiaObject):
                  market_penetration: float,
                  percentage_profit_center: float,
                  percentage_cost_center: float,
+                 sale_annual_package: float,
+                 support_annual_package: float,
+                 hiring_cost: float,
                  name:str=''):
         DessiaObject.__init__(self, name=name)
+        self.hiring_cost = hiring_cost
+        self.support_annual_package = support_annual_package
+        self.sale_annual_package = sale_annual_package
         self.percentage_cost_center = percentage_cost_center
         self.percentage_profit_center = percentage_profit_center
         self.market_penetration = market_penetration
         self.market_access_cost = market_access_cost
         self.market_maturity = market_maturity
         self.market_size = market_size
+
+    def number_sales(self, value_revenue):
+        return max(1, int(self.percentage_profit_center*value_revenue))
+
+    def number_supports(self, value_revenue):
+        return max(1, int(self.percentage_cost_center*value_revenue))
+
 
 class OperatingDivision(DessiaObject):
     _standalone_in_db = True
@@ -78,11 +140,62 @@ class OperatingDivision(DessiaObject):
     _non_hash_attributes = ['name']
 
     def __init__(self, geographic_area: GeographicArea,
-                 revenue: Revenue,
+                 revenue: Evolution,
                  name: str = ''):
         DessiaObject.__init__(self, name=name)
         self.revenue = revenue
         self.geographic_area = geographic_area
+
+        self.sales, self.supports = self.generate_employees()
+
+    def generate_employees(self):
+        years = list(self.revenue.evolutions.keys())
+        years.sort()
+        sales = []
+        supports = []
+        for y in years:
+            value_revenue = self.revenue.evolutions[y]
+            ga = self.geographic_area
+
+            number_sales = ga.number_sales(value_revenue)
+            number_sales_m = 0
+            for s in sales:
+                if s.exit_year is None:
+                    number_sales_m += 1
+            if number_sales_m < number_sales:
+                sales.extend([EmployeeSale(salary=ga.sale_annual_package,
+                                           general_expenses=0, hiring_year=y) for i in range(number_sales-number_sales_m)])
+            elif number_sales_m > number_sales:
+                number_exit = number_sales_m - number_sales
+                n = 0
+                for s in sales:
+                    if s.exit_year is None:
+                        s.exit_year = y
+                        n += 1
+                    if n == number_exit:
+                        break
+
+            number_support = ga.number_supports(value_revenue)
+            number_supports_m = 0
+            for s in supports:
+                if s.exit_year is None:
+                    number_supports_m += 1
+            if number_supports_m < number_support:
+                supports.extend([EmployeeSupport(salary=ga.support_annual_package,
+                                                 general_expenses=0, hiring_year=y) for i in range(number_support-number_supports_m)])
+            elif number_supports_m > number_support:
+                number_exit = number_supports_m - number_support
+                n = 0
+                for s in supports:
+                    if s.exit_year is None:
+                        s.exit_year = y
+                        n += 1
+                    if n == number_exit:
+                        break
+        return sales, supports
+
+    def generate_costs(self):
+        years = list(self.revenue.evolutions.keys())
 
 class MainRevenue(DessiaObject):
     _standalone_in_db = True
@@ -97,7 +210,7 @@ class MainRevenue(DessiaObject):
         self.operating_divisions = operating_divisions
 
     def revenue(self):
-        revenue = Revenue(0, 0)
+        revenue = Evolution()
         for operating_division in self.operating_divisions:
             revenue += operating_division.revenue
         return revenue
--- scripts/bm1.py
+++ scripts/bm1.py
@@ -1,30 +1,18 @@
 
-from business_plan import Bot, License, PackageLicense, Customer, Company, \
-    BusinessPlanOptimizer
+import business_plan as bp
 
-license_analysis = License(80*12, analysis_license=True)
-license_engineer = License(400*12, engineer_license=True)
-license_sdk = License(2000*12, sdk_license=True)
+ga1 = bp.GeographicArea(market_size = 100,
+                        market_maturity = 1,
+                        market_access_cost = 0.01,
+                        market_penetration= 0.004,
+                        percentage_profit_center = 0.01,
+                        percentage_cost_center = 0.001,
+                        sale_annual_package = 100000,
+                        support_annual_package = 40000,
+                        hiring_cost = 10000,
+                        name = 'France')
 
-pk_dev = PackageLicense(licenses=[license_analysis]*20+[license_engineer]*5+[license_sdk]*1)
-pk_no_dev = PackageLicense(licenses=[license_analysis]*20+[license_engineer]*5)
+rev1 = bp.Evolution({2019: 1e3, 2020: 1e3*1.2, 2021: 1e3*1.4, 2022: 1e3*1.6})
+od1 = bp.OperatingDivision(revenue=rev1, geographic_area=ga1)
+sales, supports = od1.generate_employees()
 
-bot_customer = Bot(rent_library=15e3,package_license=pk_dev,
-                   develop_by_customer=True, discount=0.4)
-bot_esn = Bot(rent_library=15e3,package_license=pk_dev,
-              develop_by_esn=True, discount=0.4)
-
-bot_dessia = Bot(rent_library=40e3,package_license=pk_no_dev,
-                 develop_by_dessia=True, discount=0.4)
-
-large_compagny = Company(large_company=True)
-middle_compagny = Company(middle_company=True)
-small_compagny = Company(small_company=True)
-
-c1 = Customer(bots=[bot_dessia]*3+[bot_customer]*2, company=large_compagny)
-
-opt1 = BusinessPlanOptimizer(number_bots=[[5, 5], [10, 25], [50, 100], [100, 250], [300, 400]],
-                             dessia_bot_ratio=[[1, 1], [0.6, 0.8], [0.6, 0.8], [0.3, 0.5], [0.2, 0.4]],
-                             revenue=[[200e3, 200e3], [800e3, 800e3], [1500e3, 2500e3], [2500e3, 4000e3], [5000e3, 8000e3]],
-                             discount_range=[[0.5, 0.8], [0.5, 0.8], [0.5, 0.8], [0.3, 0.8], [0.3, 0.8]],
-                             )
